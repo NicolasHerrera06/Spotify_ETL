c6240f7d6f826187a2c82035274674c0424cadd8
NicolasHerrera06 <herreranicolasalberto@gmail.com>
2022-08-15 03:14:51 -0300

    Se agrega el proceso de carga

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#import sqlalchemy 
-#from sqlalchemy import sessionmaker
+import sqlalchemy 
 import pandas as pd 
 import requests 
 import json 
@@ -7,9 +6,9 @@ from datetime import datetime
 import datetime 
 import sqlite3
 
-DATABASE_LOCATION = "sqlite: ///my_played_trancks.sqlite"
-USER_ID = ""
-TOKEN = ""
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+USER_ID = "Napo_06"
+TOKEN = "BQCRjSS36w8GJqd4zQWblwWRGC2weEGapIqPzGu0RGJys-fQJrhlUNa_YiuPE03xusB7eds8l3HE2UfRLU6JOda5BdESXgI--RrUv6114W-vvVD2LcwGhTEglnLZzz98-UGGVVRSxgVDNFiELXZNwnxoO20qrQ8D0a4FpUs3uqK-GdnD2WKAutj35wNmAgTh-e0"
 
 # Genera el token en: https://developer.spotify.com/console/get-recently-played/
 # Nota: Necesitas una cuenta de Spotify (puede ser una cuenta gratuita)
@@ -35,35 +34,39 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
         raise Exception("Se encontraron valores nulos")
     
     # Check that all timestamps are of yesterday's date 
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+    #yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    #yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
     
-    timestamps = df["timestamp"].tolist()
-    for timestamp in timestamps:
-        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
-            raise Exception("At least one od the returned songs does not come from within the las 24 houres")
+    #timestamps = df["timestamp"].tolist()
+    #for timestamp in timestamps:
+    #    if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+    #        raise Exception("At least one od the returned songs does not come from within the las 24 houres")
 
-    return True 
+    #return True 
 
 
 
 if __name__ == "__main__":
+
+    # Parte de extraccion del proceso ETL
+    # Extract part of the ETL process
 
     headers  = {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
         "Authorization" : "Bearer {token}".format(token=TOKEN)
     }
-
+# Convierte la hora a formate Unix in milisegundos
+# Convert time to Unix timestamp in miliseconds 
 today = datetime.datetime.now()
-yesterday = today - datetime.timedelta(days=1)
+yesterday = today - datetime.timedelta(days=60)
 yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
 r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
 
 data = r.json()
 
-print(data)
+#print(data)
 
 song_names = []
 artist_names = []
@@ -90,4 +93,29 @@ song_df = pd.DataFrame(song_dict, columns = ["song_name","artist_names","played_
 if check_if_valid_data(song_df):
     print("Validación de datos, procesando carga")
 
-#print(song_df)
+# Carga
+# Load 
+engine =  sqlalchemy.create_engine(DATABASE_LOCATION)
+conn = sqlite3.connect("my_played_tracks.sqlite")
+cursor = conn.cursor()
+
+sql_query = """
+CREATE TABLE IF NOT EXISTS my_played_tracks(
+    song_name VARCHAR(200),
+    artist_name VARCHAR(200),
+    played_at VARCHAR(200), 
+    timestamp VARCHAR(200),
+    CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+)
+"""
+
+cursor.execute(sql_query)
+print("Creación de tablas exitosa")
+
+try:
+    song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+except:
+    print("Los datos ya existen en la tabla")
+
+conn.close()
+print("Se cierra la conexión")
