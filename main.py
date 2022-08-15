09888e170bc1380d2bfb37c67082e31f0fa186c5
NicolasHerrera06 <herreranicolasalberto@gmail.com>
2022-08-15 02:24:45 -0300

    Se agregan validaciones

--- main.py
+++ main.py
@@ -8,8 +8,44 @@ import datetime
 import sqlite3
 
 DATABASE_LOCATION = "sqlite: ///my_played_trancks.sqlite"
-USER_ID = "Napo_06"
-TOKEN = "BQDByv2E1Lsf83U1qK_TYuI6dljPicFMdbeuMw3U23Peka2icqLE-8ean2xKVZRGbv8DozbTplEZF28_xn-xeaHRgPFJ1fgHrufizO6UxnezR3njqfQO_VFZRj8mqt3NukMZpgZkUp61O6j1QJ3yWi5_votbecyBEYn9qaDZEqMYbsUISOKOWxuO-sspUXWyOEA"
+USER_ID = ""
+TOKEN = ""
+
+# Genera el token en: https://developer.spotify.com/console/get-recently-played/
+# Nota: Necesitas una cuenta de Spotify (puede ser una cuenta gratuita)
+# Generate your token here: https://developer.spotify.com/console/get-recently-played/
+# Note: You need a Spotify account (can ve easily created for free) 
+
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Verifica si el dataframe está vacío
+    #Check if dataframe is empty
+    if df.empty:
+        print("No hay canciones descargadas. Termina la ejecución")
+        return False
+    # Verifica la Primary Key
+    # Primary Key Check
+    if pd.Series(df["played_at"]).is_unique:
+        pass
+    else:
+        raise Exception("Hay duplicados por violación en la verificacion de la Primary Key")
+
+    #Verificar nulos
+    #Check for nulls 
+    if df.isnull().values.any():
+        raise Exception("Se encontraron valores nulos")
+    
+    # Check that all timestamps are of yesterday's date 
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+    
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception("At least one od the returned songs does not come from within the las 24 houres")
+
+    return True 
+
+
 
 if __name__ == "__main__":
 
@@ -43,10 +79,15 @@ for song in data["items"]:
 song_dict = {
     "song_name" : song_names,
     "artist_names": artist_names,
-    "played_at_list": played_at_list,
-    "timestamps": timestamps
+    "played_at": played_at_list,
+    "timestamp": timestamps
 }
 
-song_df = pd.DataFrame(song_dict, columns = ["song_name","artist_names","played_at_list","timestamps"])
+song_df = pd.DataFrame(song_dict, columns = ["song_name","artist_names","played_at","timestamp"])
 
-print(song_df)
+# Validación
+# Validate 
+if check_if_valid_data(song_df):
+    print("Validación de datos, procesando carga")
+
+#print(song_df)
